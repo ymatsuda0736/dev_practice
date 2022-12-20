f3c11568e225d39f4c18ca9380ee2af61fcb133a
ymatsuda0736 <yuki.matsuda0736@gmail.com>
2022-12-20 09:19:10 +0900

    fin

--- 20221212-1219_gcd_coding/gmd.py
+++ 20221212-1219_gcd_coding/gmd.py
@@ -35,7 +35,6 @@ class GCDCalculator:
 
     @staticmethod
     def _get_divisors(n: int):
-        assert 1 <= n and isinstance(n, int)
         range_max = int(numpy.sqrt(abs(n)))
         pair_list = [[i, int(n / i)] for i in range(1, range_max + 1) if n % i == 0]
         return sorted(sum(pair_list, []))
