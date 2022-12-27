5b0e0ce8478794aa1e4abc1eb81e4f0ad3cb9939
ymatsuda0736 <yuki.matsuda0736@gmail.com>
2022-12-27 12:49:34 +0900

    tune

--- poker/persons.py
+++ poker/persons.py
@@ -18,11 +18,6 @@ class Player:
         # Chipが不正じゃないかなどチェック
         pass
 
-    def start_game(self, dealer):
-        if not isinstance(Dealer, dealer):
-            raise ValueError("ディーラーが不正です")
-        self.dealer = dealer
-
     def receive_cards(self, *cards):
         if any([not isinstance(Card, c) for c in cards]):
             raise ValueError("カードが不正です")
@@ -45,12 +40,8 @@ class Dealer:
         self.deck = None
         self.players = None
 
-    def start_game(self, *players):
-        if any([not isinstance(Player, p) for p in players]):
-            raise ValueError("プレイヤーが不正です")
-
+    def start_game(self):
         self.deck = Deck()
-        self.players = players
 
     def give_cards(self):
         # デッキをシャッフルして各プレイヤーにカードを配る
